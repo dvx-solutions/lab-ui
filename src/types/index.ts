651dff3189ea2451290542e17b390c3ed92f548d
viniciusoliveiras <vinitag190@gmail.com>
2022-06-30 12:41:59 -0300

    feat(types): adição de tipagens de usuário e ajuste na tipagem de empresa

--- src/types/index.ts
+++ src/types/index.ts
@@ -27,4 +27,23 @@ export interface IAPIResponse<T> {
   totalPages: number;
 }
 
+export interface IJWTDecoded {
+  fullName: string;
+  "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress": string;
+  "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier": string;
+  image_url: string;
+}
+
+export interface ILoggedUser {
+  token: string;
+  refreshToken: string;
+  refreshTokenExpiryTime: string;
+}
+
+export interface ISignInCredentials {
+  email: string;
+  password: string;
+  tenant: string;
+}
+
 export * from "./models";
