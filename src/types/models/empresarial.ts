e0a62b2102381534e352fcd4cc5cee08dfd14487
Guilherme Bilonia <guilherme.henrique@devexsolucoes.com.br>
2022-07-11 16:30:10 -0300

    ajuse na tipagem de IEmpresa

--- src/types/models/empresarial.ts
+++ src/types/models/empresarial.ts
@@ -80,28 +80,28 @@ export interface IContaContabil {
   id: number;
   planoId: number;
   superiorId: number;
-  codigo:  string,
-  nome:  string,
-  codigoReduzido:  string,
-  codigoMascara:  string,
-  descricao:  string,
-  origemConta: 1,
+  codigo: string;
+  nome: string;
+  codigoReduzido: string;
+  codigoMascara: string;
+  descricao: string;
+  origemConta: 1;
   tipoUso: number;
   tipoConta: number;
   naturezaContaId: number;
   agrupadorId: number;
   nivel: number;
   analitico: true;
-  codigoNivel1:  string;
-  codigoNivel2:  string;
-  codigoNivel3:  string;
-  codigoNivel4:  string;
-  codigoNivel5:  string;
-  nomeNivel1:  string;
-  nomeNivel2:  string;
-  nomeNivel3:  string;
-  nomeNivel4:  string;
-  nomeNivel5:  string;
+  codigoNivel1: string;
+  codigoNivel2: string;
+  codigoNivel3: string;
+  codigoNivel4: string;
+  codigoNivel5: string;
+  nomeNivel1: string;
+  nomeNivel2: string;
+  nomeNivel3: string;
+  nomeNivel4: string;
+  nomeNivel5: string;
   inicioValidade: string;
   terminoValidade: string;
   ativo: true;
@@ -113,7 +113,7 @@ export interface IEmpresa {
   id: number;
   nome: string;
   situacao: number;
-  subordinadas: string[];
+  subordinadas: IEmpresa[];
   superiorId: number;
 }
 
