531b42d296bdaaab2361c9e84eb61d17df6e1aaf
viniciusoliveiras <vinitag190@gmail.com>
2022-07-26 18:20:05 -0300

    feat(react-query unidade-negocio): criação da query

--- src/hooks/react-query/empresarial/index.ts
+++ src/hooks/react-query/empresarial/index.ts
@@ -1,3 +1,4 @@
 export * from './plano';
 export * from './unidade-centro';
+export * from './unidade-negocio';
 export * from './unidade';
---
+++ src/hooks/react-query/empresarial/unidade-negocio.ts
@@ -0,0 +1,54 @@
+import { AxiosInstance } from 'axios';
+import { useQuery } from 'react-query';
+
+import {
+  convertAdvancedSearchToReactQueryKeys,
+  getReactQueryPaginationKeys,
+} from '+/lib';
+import {
+  IAPIPaginatedResponse,
+  IBodyRequest,
+  IUnidadeNegocio,
+  TSelectOption,
+} from '+/types';
+
+interface IProps {
+  advancedSearch?: IBodyRequest<keyof IUnidadeNegocio>['advancedSearch'];
+  API_Instance: AxiosInstance;
+  pageNumber?: IBodyRequest['pageNumber'];
+  pageSize?: IBodyRequest['pageSize'];
+}
+
+export function useUnidadeNegocio({
+  advancedSearch,
+  API_Instance,
+  pageNumber = 1,
+  pageSize = 25,
+}: IProps) {
+  return useQuery(
+    [
+      'unidade-negocio',
+      convertAdvancedSearchToReactQueryKeys(advancedSearch),
+      getReactQueryPaginationKeys(pageNumber, pageSize),
+    ],
+    async () => {
+      const payload: IBodyRequest = {
+        advancedSearch,
+        orderBy: ['nomeFantasia'],
+        pageNumber,
+        pageSize,
+      };
+
+      const { data } = await API_Instance.post<
+        IAPIPaginatedResponse<IUnidadeNegocio[]>
+      >('empresarial/unidades-negocios/listar', payload);
+
+      const options: TSelectOption[] = data.data.map(x => ({
+        text: `${x.codigo} - ${x.nomeFantasia}`,
+        value: x.id,
+      }));
+
+      return { ...data, options };
+    }
+  );
+}
