8013f2b82201a1d7c6acef8c2ea4cad00702d87c
viniciusoliveiras <vinitag190@gmail.com>
2022-07-29 11:52:26 -0300

    v1.0.1

--- src/types/react-query.ts
+++ src/types/react-query.ts
@@ -5,8 +5,8 @@ import { IBodyRequest } from '+/types/axios';
 export interface IQueryParams<T> {
   advancedSearch?: IBodyRequest<T>['advancedSearch'];
   API_Instance: AxiosInstance;
-  keyword: string;
-  orderBy: T[];
+  keyword?: string;
+  orderBy?: T[];
   pageNumber?: IBodyRequest['pageNumber'];
   pageSize?: IBodyRequest['pageSize'];
 }
