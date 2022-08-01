9b4e89eb079e24bca84bb42384dc1e93120f61a1
Guilherme Bilonia <guilherme.henrique@devexsolucoes.com.br>
2022-08-01 11:41:31 -0300

    refactor(default type): default type aplicado em unidade
    hook de unidade ja utilizando novo padrao de parametros

---
+++ src/hooks/react-query/unidade.ts
@@ -0,0 +1,47 @@
+import { useQuery } from '@tanstack/react-query';
+import { AxiosInstance } from 'axios';
+
+import { convertAdvancedSearchToReactQueryKeys } from '+/lib/formatters';
+import { IQueryParams } from '+/types';
+import { IAPIPaginatedResponse, IBodyRequest } from '+/types/axios';
+import { IUnidade } from '+/types/models/empresarial';
+
+interface IUseUnidades extends IQueryParams<keyof IUnidade> {
+  empresaAnoFiscalId: number;
+  planoId: number;
+}
+
+export const useUnidades = ({
+  API_Instance,
+  planoId,
+  advancedSearch,
+  pageNumber = 1,
+  empresaAnoFiscalId,
+  pageSize = 9999,
+}: IUseUnidades) => {
+  return useQuery(
+    [
+      'unidades',
+      `plano-id-${planoId}`,
+      `empresa-ano-fiscal-${empresaAnoFiscalId}`,
+      convertAdvancedSearchToReactQueryKeys(advancedSearch),
+      `pageNumber-${pageNumber}`,
+      `pageSize-${pageSize}`,
+    ],
+    async () => {
+      const { data } = await API_Instance.post<
+        IAPIPaginatedResponse<IUnidade[]>
+      >('empresarial/unidades/listar', {
+        pageNumber,
+        pageSize,
+        empresaAnoFiscalId,
+        advancedSearch: [
+          advancedSearch ? { ...advancedSearch } : {},
+          { fields: ['planoId'], keyword: planoId.toString() },
+        ],
+      } as IBodyRequest<keyof IUnidade>);
+
+      return data;
+    }
+  );
+};
