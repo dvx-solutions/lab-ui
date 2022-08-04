74d3a0cb435577b0c06f5323c7eb08e359f7c5f5
viniciusoliveiras <vinitag190@gmail.com>
2022-08-04 13:22:22 -0300

    v1.2.6

--- src/hooks/react-query/colaboradores/colaborador.ts
+++ src/hooks/react-query/colaboradores/colaborador.ts
@@ -1,42 +1,58 @@
 import { useQuery } from '@tanstack/react-query';
-import { AxiosInstance } from 'axios';
 
-import { convertAdvancedSearchToReactQueryKeys } from '+/lib/formatters';
+import {
+  convertAdvancedSearchToReactQueryKeys,
+  getReactQueryPaginationKeys,
+} from '+/lib/formatters';
+import { IQueryParams } from '+/types';
 import { IAPIPaginatedResponse, IBodyRequest } from '+/types/axios';
 import { IColaborador } from '+/types/models/colaboradores';
 
-interface IUseColaboradores {
-  advancedSearch?: IBodyRequest['advancedSearch'];
-  API_Instance: AxiosInstance;
+interface Props extends IQueryParams<keyof IColaborador> {
+  cargoId?: number;
   empresaId: number;
-  pageNumber?: number;
-  pageSize?: number;
+  setorId?: number;
+  unidadeNegocioId?: number;
 }
 
 export const useColaboradores = ({
   advancedSearch,
   API_Instance,
+  cargoId,
   empresaId,
+  keyword,
+  orderBy,
   pageNumber = 1,
-  pageSize = 50,
-}: IUseColaboradores) => {
+  pageSize = 25,
+  setorId,
+  unidadeNegocioId,
+}: Props) => {
   return useQuery(
     [
       'colaboradores',
-      `empresa-${empresaId}`,
-      `page-number-${pageNumber}`,
-      `page-size-${pageSize}`,
+      `empresaId-${empresaId}`,
+      `cargoId-${cargoId}`,
+      `setorId-${setorId}`,
+      `unidadeNegocioId-${unidadeNegocioId}`,
       convertAdvancedSearchToReactQueryKeys(advancedSearch),
+      getReactQueryPaginationKeys(pageNumber, pageSize),
     ],
     async () => {
-      const { data } = await API_Instance.post<
-        IAPIPaginatedResponse<IColaborador[]>
-      >('colaboradores/colaboradores/listar', {
+      const payload = {
+        advancedSearch,
+        cargoId,
+        empresaId,
+        keyword,
+        orderBy,
         pageNumber,
         pageSize,
-        advancedSearch,
-        empresaId,
-      } as IBodyRequest<keyof IColaborador>);
+        setorId,
+        unidadeNegocioId,
+      } as IBodyRequest;
+
+      const { data } = await API_Instance.post<
+        IAPIPaginatedResponse<IColaborador[]>
+      >('colaboradores/colaboradores/listar', payload);
 
       return data;
     },
