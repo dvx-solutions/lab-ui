a9a0719f1eab5ce014a1900dfd05e855fa8f93e5
viniciusoliveiras <vinitag190@gmail.com>
2022-08-08 15:33:05 -0300

    v1.4.1

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,4 @@
-/* eslint-disable import/no-extraneous-dependencies */
-/* eslint-disable @typescript-eslint/no-var-requires */
-
 /** @type {import('tailwindcss').Config} */
-const colors = require('tailwindcss/colors');
 
 module.exports = {
   jit: true,
@@ -12,8 +8,8 @@ module.exports = {
       fontFamily: ['Poppins'],
       colors: {
         brand: {
-          primary: colors.blue['500'],
-          secondary: colors.blue['300'],
+          primary: 'inherit',
+          secondary: 'inherit',
         },
       },
     },
