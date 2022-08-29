3cea5213463ae4ae23e37262d83ae745ce2be55d
viniciusoliveiras <vinitag190@gmail.com>
2022-08-29 17:06:44 -0300

    docs(changeset): abstração do Dialog

---
+++ src/components/dialogs/Dialog.tsx
@@ -0,0 +1,48 @@
+import { Root, Overlay, Content, Title, Close } from '@radix-ui/react-dialog';
+import { ReactNode } from 'react';
+import { FiX } from 'react-icons/fi';
+
+import { Button } from '+/components/buttons/Button';
+
+export interface DialogProps {
+  children: ReactNode;
+  title: string;
+  isOpen: boolean;
+  onClose: () => void;
+  onOpen: () => void;
+}
+
+export function Dialog({
+  children,
+  isOpen,
+  onClose,
+  onOpen,
+  title,
+}: DialogProps) {
+  const handleStateChange = (state: boolean) => {
+    if (state) {
+      onOpen();
+    } else {
+      onClose();
+    }
+  };
+
+  return (
+    <Root open={isOpen} onOpenChange={handleStateChange}>
+      <Overlay className="fixed inset-0 bg-black/25" />
+
+      <Content className="fixed top-2/4 left-2/4 w-fit min-w-[25vw] -translate-y-2/4 -translate-x-2/4 rounded bg-white p-4 shadow-md">
+        <Title className="mb-4 flex items-center justify-between gap-16 text-xl font-medium">
+          {title}
+          <Close>
+            <Button className="rounded-full p-1 transition-all duration-300 hover:bg-brand-primary hover:text-brand-text-primary">
+              <FiX />
+            </Button>
+          </Close>
+        </Title>
+
+        {children}
+      </Content>
+    </Root>
+  );
+}
--- src/components/dialogs/pessoas/CUPessoaJuridica.tsx
+++ src/components/dialogs/pessoas/CUPessoaJuridica.tsx
@@ -1,13 +1,19 @@
 import { zodResolver } from '@hookform/resolvers/zod';
-import * as Dialog from '@radix-ui/react-dialog';
 import { useQuery } from '@tanstack/react-query';
 import { AxiosError, AxiosInstance } from 'axios';
 import { useEffect } from 'react';
 import { useForm } from 'react-hook-form';
-import { FiX } from 'react-icons/fi';
 import { z } from 'zod';
 
-import { Button, Input, Select, Checkbox } from '+/components';
+import {
+  Button,
+  Checkbox,
+  Dialog,
+  DialogProps,
+  Input,
+  Select,
+  Spinner,
+} from '+/components';
 import { customErrorMap, convertEnumToSelectOptions } from '+/lib';
 import {
   EPessoaContribuinte,
@@ -29,12 +35,9 @@ const schema = z.object({
 
 export type TCriarPJFormValues = z.infer<typeof schema>;
 
-export interface CUPessoaJuridicaProps {
+export interface CUPessoaJuridicaProps extends DialogProps {
   axiosInstance: AxiosInstance;
   dialogTitle?: string;
-  isOpen: boolean;
-  onClose: () => void;
-  onOpen: () => void;
   onSubmitError: (error: AxiosError) => Promise<void> | void;
   onSubmitSuccess: () => Promise<void>;
   registryIdToEdit?: number;
@@ -50,9 +53,8 @@ export function CUPessoaJuridica({
   onSubmitSuccess,
   registryIdToEdit = -1,
 }: CUPessoaJuridicaProps) {
-  const { data: registerToEdit } = useQuery(
-    ['pessoas-juridicas', `id-${registryIdToEdit}`],
-    async () => {
+  const { data: registerToEdit, isFetching: isFetchingRegisterToEdit } =
+    useQuery(['pessoas-juridicas', `id-${registryIdToEdit}`], async () => {
       if (registryIdToEdit <= 0) return null;
 
       const { data: res } = await axiosInstance.get<
@@ -60,8 +62,7 @@ export function CUPessoaJuridica({
       >(`pessoas/pessoas-juridicas/${registryIdToEdit}`);
 
       return res.data;
-    }
-  );
+    });
 
   const {
     formState: { errors, isSubmitting },
@@ -72,15 +73,6 @@ export function CUPessoaJuridica({
   } = useForm<TCriarPJFormValues>({
     resolver: zodResolver(schema),
   });
-
-  const handleStateChange = (state: boolean) => {
-    if (state) {
-      onOpen();
-    } else {
-      onClose();
-      reset();
-    }
-  };
 
   const onFormSubmit = async (values: TCriarPJFormValues) => {
     await axiosInstance
@@ -108,19 +100,18 @@ export function CUPessoaJuridica({
   }, [registerToEdit, isOpen]);
 
   return (
-    <Dialog.Root open={isOpen} onOpenChange={handleStateChange}>
-      <Dialog.Overlay className="fixed inset-0 bg-black/25" />
-
-      <Dialog.Content className="fixed top-2/4 left-2/4 w-fit min-w-[25vw] -translate-y-2/4 -translate-x-2/4 rounded bg-white p-4 shadow-md">
-        <Dialog.Title className="mb-4 flex items-center justify-between gap-16 text-xl font-medium">
-          {dialogTitle}
-          <Dialog.Close>
-            <Button className="rounded-full p-1 transition-all duration-300 hover:bg-brand-primary hover:text-brand-text-primary">
-              <FiX />
-            </Button>
-          </Dialog.Close>
-        </Dialog.Title>
-
+    <Dialog
+      isOpen={isOpen}
+      onClose={() => {
+        onClose();
+        reset();
+      }}
+      onOpen={onOpen}
+      title={dialogTitle}
+    >
+      {isFetchingRegisterToEdit && registryIdToEdit > 0 ? (
+        <Spinner />
+      ) : (
         <form
           className="grid min-w-[40vw] grid-cols-2 items-end rounded bg-white"
           onSubmit={handleSubmit(onFormSubmit)}
@@ -169,7 +160,7 @@ export function CUPessoaJuridica({
             </Button>
           </div>
         </form>
-      </Dialog.Content>
-    </Dialog.Root>
+      )}
+    </Dialog>
   );
 }
