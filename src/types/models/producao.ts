76d7dcf6cd5d749de760080a12590489075aaaf1
Guilherme Bilonia <guilherme.henrique@devexsolucoes.com.br>
2022-09-05 15:59:11 -0300

    feat(types/producao): nova timpagem IOdemServico adicionada

--- src/types/models/producao.ts
+++ src/types/models/producao.ts
@@ -36,3 +36,41 @@ export interface ITema {
   terminoValidade: string;
   ativo: boolean;
 }
+
+export interface IOrdemServico {
+  id: number;
+  tipoOrdemServicoId: number;
+  codigo: string;
+  titulo: string;
+  descricao: string;
+  seq: number;
+  nivel: number;
+  analitico: boolean;
+  superiorId: number;
+  temaId: number;
+  empresaId: number;
+  unidadeNegocioId: number;
+  contratoItemId: number;
+  propostaItemId: number;
+  clienteId: number;
+  contatoId: number;
+  projetoAtividadeId: number;
+  quadroId: number;
+  statusId: number;
+  tipoTerefaId: number;
+  equipeId: number;
+  responsavelId: number;
+  dataInicioPrevisto: string;
+  dataTerminoPrevisto: string;
+  horaInicioPrevisto: string;
+  horaTerminoPrevisto: string;
+  tempoPrevisto: number;
+  dataInicioRealizado: string;
+  dataTerminoRealizado: string;
+  horaInicioRealizado: string;
+  horaTerminoRealizado: string;
+  tempoRealizado: number;
+  percentualExecucao: number;
+  percentualPrazo: number;
+  situacao: number;
+}
