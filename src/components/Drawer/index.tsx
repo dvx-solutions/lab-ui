fa56c28e64c563378ab16419c97b6db3afdeeb30
AndersonDev22 <luizandersondev32@gmail.com>
2022-10-06 18:23:55 -0300

    create component Drawer

---
+++ src/components/Drawer/index.tsx
@@ -0,0 +1,26 @@
+import * as Dialog from '@radix-ui/react-dialog';
+
+export function DrawerDevex() {
+  return (
+    <Dialog.Root>
+      <Dialog.Trigger asChild>
+        {/* eslint-disable-next-line react/button-has-type */}
+        <button className="mt-[18px] w-full rounded-xl bg-blue-600 p-3 text-sm text-white hover:bg-blue-700">
+          Abrir Drawer
+        </button>
+      </Dialog.Trigger>
+      <Dialog.Portal>
+        <Dialog.Overlay className="fixed inset-0 h-screen w-screen bg-black bg-opacity-[0.7]" />
+        {/* 
+        className="positionLeft"- para abrir a esquerda
+        className="positionRight"- para abrir a direita
+        className="positionTop"- para abrir no topo
+        className="positionTop"- para abrir embaixo 
+        */}
+        <Dialog.Content className="positionLeft">
+          O CONTEÚDO DO DRAWER VAI AQUI DENTRO
+        </Dialog.Content>
+      </Dialog.Portal>
+    </Dialog.Root>
+  );
+}
