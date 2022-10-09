000082dadaa47690edbd468182e37d331fc54fcd
AndersonDev22 <luizandersondev32@gmail.com>
2022-10-08 23:41:53 -0300

    add file drawer stitches

---
+++ src/components/Drawer/DrawerStitches/index.tsx
@@ -0,0 +1,26 @@
+import * as Dialog from '@radix-ui/react-dialog';
+// import type * as Stitches from '@stitches/react';
+
+// import { StyledContent, StyledOverlay } from './styles';
+
+// type StyledContentVariants = Stitches.VariantProps<typeof StyledContent>;
+
+// interface IContentProps {
+//   openingPosition?: 'left' | 'right' | 'top' | 'bottom';
+// }
+
+// export function Drawer({
+//   openingPosition = 'left',
+// }: IContentProps & StyledContentVariants) {
+//   return (
+//     <Dialog.Root>
+//       <Dialog.Trigger>Abrir Drawer</Dialog.Trigger>
+//       <StyledOverlay />
+//       <StyledContent openingPosition={openingPosition}>
+//         <div>
+//           <h1>Drawer Content</h1>
+//         </div>
+//       </StyledContent>
+//     </Dialog.Root>
+//   );
+// }
---
+++ src/components/Drawer/DrawerStitches/styles.ts
@@ -0,0 +1,74 @@
+import * as Dialog from '@radix-ui/react-dialog';
+
+// import { keyframes, styled } from '../../styles';
+
+// const overlayShowLeft = keyframes({
+//   '0%': { transform: 'translateX(0)' },
+//   '100%': { transform: 'translateX(100%)' },
+// });
+
+// const overlayShowRight = keyframes({
+//   '0%': { transform: 'translateX(100%)' },
+//   '100%': { transform: 'translateX(0)' },
+// });
+
+// const overlayShowTop = keyframes({
+//   '0%': { transform: 'translateY(0)' },
+//   '100%': { transform: 'translateY(100%)' },
+// });
+
+// const overlayShowBottom = keyframes({
+//   '0%': { transform: 'translateY(100%)' },
+//   '100%': { transform: 'translateY(0)' },
+// });
+
+// export const StyledOverlay = styled(Dialog.Overlay, {
+//   display: 'flex',
+//   width: '100%',
+//   height: '100%',
+//   position: 'fixed',
+//   inset: 0,
+//   background: 'rgb(0, 0, 0, 0.75)',
+//   opacity: '0.7',
+// });
+
+// export const StyledContent = styled(Dialog.Content, {
+//   display: 'flex',
+//   width: '25rem',
+//   height: '100%',
+//   background: '$white',
+//   position: 'fixed',
+//   color: '$black',
+//   justifyContent: 'center',
+//   alignItems: 'center',
+
+//   variants: {
+//     openingPosition: {
+//       left: {
+//         top: 0,
+//         left: '-25rem',
+//         animation: `${overlayShowLeft} 400ms linear forwards`,
+//       },
+
+//       right: {
+//         top: 0,
+//         right: 0,
+//         animation: `${overlayShowRight} 400ms linear forwards`,
+//       },
+
+//       top: {
+//         top: '-15rem',
+//         width: '100%',
+//         height: '15rem',
+//         animation: `${overlayShowTop} 400ms linear forwards`,
+//       },
+
+//       bottom: {
+//         bottom: 0,
+//         width: '100%',
+//         height: '15rem',
+//         animation: `${overlayShowBottom} 400ms linear forwards`,
+//       },
+//     },
+//   },
+// });
